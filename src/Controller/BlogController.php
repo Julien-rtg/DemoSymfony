ea74d03edb3053e57d391808788a211c7bd8d10b
Julien-rtg <julien.rittl@gmail.com>
2020-10-01 15:47:12 +0200

    add article in database and display

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,16 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
-    {
+    public function index(ArticleRepository $repo){
+
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
 
@@ -21,13 +26,22 @@ class BlogController extends AbstractController
      * @Route("/", name="home")
      */
     public function home(){
-         return $this->render('blog/home.html.twig');
+        return $this->render('blog/home.html.twig');
     }
 
     /**
      * @Route("/blog/new", name="newArticle")
      */
     public function new(){
-         return $this->render('blog/newArticle.html.twig');
+        return $this->render('blog/newArticle.html.twig');
+    }
+
+    /**
+     * @Route("/blog/article/{id}", name="blogShow")
+     */
+    public function show(Article $article){
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
     }
 }
